7106ab3edff845f15e2fa86dd4242fcb0304e669
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-11-03 11:31:30 +0500

    1020

--- Slicing_list10.py
+++ Slicing_list10.py
@@ -7,6 +7,6 @@ def main(list1,n):
     Returns:
         list: return answer.
     """ 
-    if n<len(list1):
+    if n<len(list1) and n>0:
      return list1[-1:n:-1]
 print(main([1,2,3,4,5,6,7,8,9],4))
